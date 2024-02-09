0aba9bfb90989342adb1e94cf0bed41c48d185b0
Volodymyr Petrenko <mailbox.github@gmail.com>
2024-02-09 17:07:47 +0200

    corrected two

--- task_two.py
+++ task_two.py
@@ -7,10 +7,10 @@ def generator_numbers(text: str):
             continue
 
 
-def sum_profit(text):
-    print(sum(list(generator_numbers(text))))
+def sum_profit(text, func):
+    print(sum(list(func(text))))
 
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 
-sum_profit(text)
+sum_profit(text, generator_numbers)
